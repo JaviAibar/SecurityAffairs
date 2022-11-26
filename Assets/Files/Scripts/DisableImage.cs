239f495e66bbeaa1bd26d6a543a20f9b4e13ee99
Javier Aibar <es.javi.es@gmail.com>
2022-11-26 16:51:45 +0100

    Added final work (explained in last commit) Minor bug fix

---
+++ Assets/Files/Scripts/DisableImage.cs
@@ -0,0 +1,12 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableImage : MonoBehaviour
+{
+    public void SetImageDisable()
+    {
+        gameObject.SetActive(false);
+    }
+}
